6b14baee17642aade9902461e14f407adb89a588
matt <matt@mrstoffel.com>
2024-11-26 11:37:27 -0800

    set up noice

--- home/nvim.nix
+++ home/nvim.nix
@@ -2,29 +2,54 @@
 
 {
   home-manager.users.matt = {
-    programs = {
-      neovim = {
-        enable = true;
-        
-	viAlias = true;
-	vimAlias = true;
-	vimdiffAlias = true;
+    programs.neovim = 
+    let
+      toLua = str: "lua << EOF\n${str}\nEOF\n";
+      toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+    in
+    {
+      enable = true;
+      
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
 
-	plugins = with pkgs.vimPlugins; [
-	  conform-nvim
-	  dracula-nvim
-	  neo-tree-nvim
-	  nvim-lint
-	  nvim-treesitter
-	  nvim-treesitter-textobjects
-	  lualine-nvim
-	  noice-nvim
-	  persistence-nvim
-	  telescope-nvim
-	  tokyonight-nvim
-	  which-key-nvim
-	];
-      };
+      plugins = with pkgs.vimPlugins; [
+	{
+	  plugin = comment-nvim;
+	  config = toLua "require(\"Comment\").setup()";
+	}
+	{
+          plugin = conform-nvim;
+	  config = toLuaFile ./nvim/plugin/conform.lua;
+	}
+	{
+          plugin = dracula-nvim;
+	  config = "colorscheme dracula";
+	}
+        neo-tree-nvim
+        nvim-cmp
+        nvim-lint
+        nvim-lspconfig
+        (nvim-treesitter.withPlugins (p: [
+          p.tree-sitter-nix
+          p.tree-sitter-vim
+          p.tree-sitter-bash
+          p.tree-sitter-lua
+          p.tree-sitter-python
+          p.tree-sitter-json
+        ]))
+        nvim-treesitter-textobjects
+        lualine-nvim
+	{
+          plugin = noice-nvim;
+	  config = toLuaFile ./nvim/plugin/noice.lua;
+	}
+        persistence-nvim
+        telescope-nvim
+        tokyonight-nvim
+        which-key-nvim
+      ];
     };
   };
 }
