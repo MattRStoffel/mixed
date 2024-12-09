b46966da02661d5a10d17c2fe4171cac76dd1cf0
matt <matt@mrstoffel.com>
2024-12-09 11:03:34 -0800

    176 current  2024-12-09 11:03:19  25.05.20241201.ac35b10  6.11.7                          *

--- home/nvim/plugins/lspconfig.lua
+++ home/nvim/plugins/lspconfig.lua
@@ -1,5 +1,33 @@
 local lspc = require("lspconfig")
 
-lspc.lua_lsp.setup{}
+-- List of language servers to configure
+local servers = {
+  pyright = {},           -- Python
+  lua_ls = {},            -- Lua (use "sumneko_lua" for older versions)
+  nil_ls = {},            -- Nix
+  clangd = {},            -- C, C++
+  bashls = {},            -- Shell scripting
+  gopls = {},             -- Go
+  texlab = {},            -- LaTeX
+}
 
-lspc.gopls.setup{}
+-- Default options for all servers
+local default_opts = {
+  on_attach = function(_, bufnr)
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  end,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+}
+
+-- Setup each server with default options
+for server, opts in pairs(servers) do
+  lspc[server].setup(vim.tbl_deep_extend("force", default_opts, opts))
+end
+
