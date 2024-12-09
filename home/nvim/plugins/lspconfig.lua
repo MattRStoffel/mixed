e7f2615e8d3de5b18d933b77f731d1b0be076c13
matt <matt@mrstoffel.com>
2024-12-09 12:15:15 -0800

    189 current  2024-12-09 12:15:02  25.05.20241201.ac35b10  6.11.7                          *

--- home/nvim/plugins/lspconfig.lua
+++ home/nvim/plugins/lspconfig.lua
@@ -14,7 +14,7 @@ local servers = {
 
 -- Default options for all servers
 local default_opts = {
-  capabilities = capabilities
+  capabilities = capabilities,
   on_attach = function()
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer=0})
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer=0})
