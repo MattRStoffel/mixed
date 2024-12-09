d270af2eda7fc42a3754b64b0e72743cfd450834
matt <matt@mrstoffel.com>
2024-12-09 11:10:36 -0800

    178 current  2024-12-09 11:10:24  25.05.20241201.ac35b10  6.11.7                          *

--- home/nvim/plugins/lspconfig.lua
+++ home/nvim/plugins/lspconfig.lua
@@ -13,15 +13,9 @@ local servers = {
 
 -- Default options for all servers
 local default_opts = {
-  on_attach = function(_, bufnr)
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  on_attach = function()
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
   end,
 }
 
