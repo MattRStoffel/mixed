ecc143b156308cc3bf1699e52d1eb98689f56936
matt <matt@mrstoffel.com>
2024-12-09 15:51:35 -0800

    197 current  2024-12-09 15:51:23  25.05.20241201.ac35b10  6.11.7                          *

--- home/nvim/plugins/lspconfig.lua
+++ home/nvim/plugins/lspconfig.lua
@@ -14,13 +14,13 @@ local servers = {
 -- Default options for all servers
 local default_opts = {
   capabilities = require('cmp_nvim_lsp').default_capabilities(), -- Need nvim-cmp installed for this
-  on_attach = function()
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer=0})
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer=0})
-    vim.keymap.set('n', 'gt', vim.lsp.buf.type_defenition, {buffer=0})
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {buffer=0})
-    vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>', {buffer=0})
-    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {buffer=0})
+  on_attach = function(_, bufnum)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer=bufnum})
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer=bufnum})
+    vim.keymap.set('n', 'gt', vim.lsp.buf.type_defenition, {buffer=bufnum})
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {buffer=bufnum})
+    vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>', {buffer=bufnum})
+    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {buffer=bufnum})
   end,
 }
 
