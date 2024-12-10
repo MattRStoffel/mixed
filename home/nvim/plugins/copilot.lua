f78c3f393da63d9a914a6576fc0a7770871bb021
matt <matt@mrstoffel.com>
2024-12-10 08:36:40 -0800

    207 current  2024-12-10 08:36:24  25.05.20241201.ac35b10  6.11.7                          *

--- home/nvim/plugins/copilot.lua
+++ home/nvim/plugins/copilot.lua
@@ -1,6 +1,6 @@
 require('copilot').setup({
   panel = {
-    enabled = false,
+    enabled = true,
     auto_refresh = false,
     keymap = {
       jump_prev = "[[",
@@ -15,7 +15,7 @@ require('copilot').setup({
     },
   },
   suggestion = {
-    enabled = false,
+    enabled = true,
     auto_trigger = false,
     hide_during_completion = true,
     debounce = 75,
