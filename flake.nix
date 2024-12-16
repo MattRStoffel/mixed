b16d1d76a0e513bf9a349b4ef71decd850245dcf
matt <matt@mrstoffel.com>
2024-12-16 13:37:52 -0800

    266 current  2024-12-16 13:37:37  25.05.20241201.ac35b10  6.11.7                          *

--- flake.nix
+++ flake.nix
@@ -5,14 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hardware.url = "github:nixos/nixos-hardware";
     home.url = "github:nix-community/home-manager";
+    xencelabs.url = "github:nilp0inter/nixos-xencelabs";
   };
 
-  outputs = {nixpkgs, hardware, home, ...} : {
+  outputs = {nixpkgs, hardware, home, xencelabs, ...} : {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [
 	hardware.nixosModules.apple-t2
         ./configuration.nix
 	home.nixosModules.home-manager
+	xencelabs.nixosModules.xencelabs
       ];
     };
   };
