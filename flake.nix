7309f9eb5c7e9959d99e5423facc38659fb03e86
matt <matt@mrstoffel.com>
2024-12-27 07:50:50 -0800

    276 current  2024-12-22 08:51:30  25.05.20241201.ac35b10  6.11.7                          *

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hardware.url = "github:nixos/nixos-hardware";
     home.url = "github:nix-community/home-manager";
+     ghostty = { url = "github:ghostty-org/ghostty"; };
   };
 
-  outputs = {nixpkgs, hardware, home, ...} : {
+  outputs = {nixpkgs, hardware, home, ghostty, ...} : {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [
 	hardware.nixosModules.apple-t2
