8131b4c343e3de18e3a6866b7d3d8b5e5951d8dc
matt <matt@mrstoffel.com>
2025-02-01 09:27:25 -0800

    303 current  2025-02-01 09:27:08  25.05.20250108.bffc22e  6.12.4                          *

--- nvidia.nix
+++ nvidia.nix
@@ -36,7 +36,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
 
     # Use Nvidia Prime to choose which GPU (iGPU or eGPU) to use.
     prime = {
