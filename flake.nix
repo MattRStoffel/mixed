10f0f200cf918421caf3cb1e1b361e4a91856389
MattRStoffel <Matt@MrStoffel.com>
2025-04-27 12:05:18 -0700

    add darwin

--- flake.nix
+++ flake.nix
@@ -2,20 +2,42 @@
   description = "nixos config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    hardware.url = "github:nixos/nixos-hardware";
-    home.url = "github:nix-community/home-manager";
+    hardware.url = "github:NixOS/nixos-hardware";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     ghostty.url = "github:ghostty-org/ghostty";
   };
-  outputs = {nixpkgs, hardware, home, ghostty, ...} @inputs: 
-    {
+  outputs = {
+    self,
+    nixpkgs,
+    nix-darwin,
+    home-manager,
+    hardware,
+    ghostty,
+    ...
+  } @ inputs: {
+    darwinConfigurations.macbook = nix-darwin.lib.darwinSystem {
+      system = "x86_64-darwin";
+      modules = [
+        ./darwin
+        home-manager.darwinModules.home-manager
+      ];
+      specialArgs = {
+        inherit inputs;
+        self = self;
+      };
+    };
+
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [
-	hardware.nixosModules.apple-t2
         ./configuration.nix
-	home.nixosModules.home-manager
+        hardware.nixosModules.apple-t2
+        home-manager.nixosModules.home-manager
       ];
-      specialArgs = { inherit inputs; };
+      specialArgs = {inherit inputs;};
     };
   };
 }
