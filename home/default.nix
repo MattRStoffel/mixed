943682f434b48b344a57faf76f2a940de0065d77
MattRStoffel <Matt@MrStoffel.com>
2025-04-29 20:26:16 -0700

    broke

--- home/default.nix
+++ home/default.nix
@@ -6,7 +6,24 @@
   home-manager.users.matt = {
     imports = [
       ./fonts.nix
+      ./direnv.nix
+      ./git.nix
+      ./nvim
+      ./starship.nix
+      # ./steam.nix
+      ./zoxide.nix
+      ./zsh.nix
     ];
+    programs = {
+      bat.enable = true;
+      lsd.enable = true;
+      fzf.enable = true;
+      fd.enable = true;
+      ripgrep.enable = true;
+      fastlane.enable = true;
+      btop.enable = true;
+    };
+
     home = {
       stateVersion = "24.11";
       shellAliases = {
