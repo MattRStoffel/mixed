415c761fc2aa0b06c1ede6d9bc8e559f5622baf8
MattRStoffel <Matt@MrStoffel.com>
2026-05-17 10:15:25 -0700

    improve thing

--- home/games/noita-entangled-worlds.nix
+++ home/games/noita-entangled-worlds.nix
@@ -9,15 +9,13 @@ let
       sha256 = "0kb0q6nyjd5hq59rgbaxjnlkml3yplrqxi40fhkird3awkqrh91f";
     };
 
-    nativeBuildInputs = [ pkgs.unzip pkgs.makeWrapper ];
+    nativeBuildInputs = [ pkgs.unzip ];
 
     unpackPhase = "unzip $src";
 
     installPhase = ''
-      install -Dm755 noita_proxy.x86_64 $out/libexec/noita_proxy
-      install -Dm644 libsteam_api.so $out/lib/libsteam_api.so
-      makeWrapper $out/libexec/noita_proxy $out/bin/noita-proxy \
-        --prefix LD_LIBRARY_PATH : $out/lib
+      install -Dm755 noita_proxy.x86_64 $out/bin/noita-proxy
+      install -Dm644 libsteam_api.so $out/bin/libsteam_api.so
     '';
   };
 in {
